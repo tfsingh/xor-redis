d407b150bce4d58faf47df31b9aa3df6eb634e1c
Tej Singh <tejfsingh@gmail.com>
2024-02-06 02:36:52 +0000

    Update benchmarks, readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # xor-redis
+
+Implementation of an [Xor filter](https://arxiv.org/pdf/1912.08258.pdf) (a probabilistic data structure which is faster/more memory efficient than Bloom/Cuckoo filters) for use in Redis instances.
+
+### Quick Start
+
+1. Run using Docker with `docker pull tfsingh/xor-redis:0.1.0 && docker run -d -p 6379:6379 tfsingh/xor-redis:0.1.0`
+
+### API
+
+####**`XOR.POPULATE`**
+
+Initializes an XOR filter with a set of entries. Overwrites existing data under the same key.
+
+Args
+
+- **Key:** Identifier for the filter.
+- **Entries:** List of objects to include.
+
+####**`XOR.CONTAINS`**
+
+Checks for an entry's presence in the filter. Like Bloom filters, may return false positives but no false negatives.
+
+Args
+
+- **Key:** Identifier for the filter.
+- **Entry:** Object to check.
+
+### Benchmarks
+
+Most code used in benchmarking/testing is available in the [evals](https://github.com/tfsingh/xor-redis/tree/main/evals) directory (memory profiling was done through redis-cli).
+
+![benchmarks](evals/bench.png)
+
+| Filter Type  | `used_memory_peak` | `used_memory_peak_human` |
+| ------------ | ------------------ | ------------------------ |
+| Xor Filter   | 6,112,496          | 5.83M                    |
+| Bloom Filter | 6,844,056          | 6.53M                    |
+
+### Acknowledgements
+
+Thank you to Thomas Graf and Daniel Lemire for discovering Xor filters, and for their [Go implementation](https://github.com/FastFilter/xorfilter/blob/master/xorfilter.go) which was used as a reference.
--- evals/bench.py
+++ evals/bench.py
@@ -6,13 +6,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 def generate_random_strings(num, length=10):
     return [''.join(random.choices(string.ascii_lowercase, k=length)) for _ in range(num)]
 
-def benchmark_xor_filter(redis_conn, num_entries):
+def benchmark_xor_filter(redis_conn, entries):
     redis_conn.flushdb()
-    entries = generate_random_strings(num_entries)
 
     start_time = time.time()
     redis_conn.execute_command('XOR.POPULATE', 'xor_filter', *entries)
@@ -25,20 +23,18 @@ def benchmark_xor_filter(redis_conn, num_entries):
 
     return insert_time, query_time
 
-def benchmark_bloom_filter(redis_conn, num_entries, error_rate=0.01):
+def benchmark_bloom_filter(redis_conn, entries, error_rate=0.01):
     redis_conn.flushdb()
-    entries = generate_random_strings(num_entries)
 
-    redis_conn.execute_command('BF.RESERVE', 'bloom_filter', str(error_rate), str(num_entries))
+    redis_conn.execute_command('BF.RESERVE', 'bloom_filter', str(error_rate), str(len(entries)))
 
     start_time = time.time()
-    for entry in entries:
-        redis_conn.execute_command('BF.ADD', 'bloom_filter', entry)
+    redis_conn.execute_command('BF.MADD', 'bloom_filter', *entries)
     insert_time = time.time() - start_time
 
     start_time = time.time()
     for entry in entries:
-        redis_conn.execute_command('BF.MEXISTS', 'bloom_filter', *entries)
+        redis_conn.execute_command('BF.EXISTS', 'bloom_filter', entry)
     query_time = time.time() - start_time
 
     return insert_time, query_time
@@ -46,9 +42,10 @@ def benchmark_bloom_filter(redis_conn, num_entries, error_rate=0.01):
 
 def run_benchmarks():
     results = []
-    for num_entries in [100, 1000]:
-        xor_insert, xor_query = benchmark_xor_filter(redis.StrictRedis(host='localhost', port=6379, db=0), num_entries)
-        bloom_insert, bloom_query = benchmark_bloom_filter(redis.StrictRedis(host='localhost', port=6380, db=0), num_entries)
+    for num_entries in [100, 500, 1000, 5000, 10000, 25000, 50000]:
+        entries = generate_random_strings(num_entries)
+        xor_insert, xor_query = benchmark_xor_filter(redis.StrictRedis(host='localhost', port=6379, db=0), entries)
+        bloom_insert, bloom_query = benchmark_bloom_filter(redis.StrictRedis(host='localhost', port=6380, db=0), entries)
 
         results.append({
             'Entries': num_entries,
@@ -72,18 +69,16 @@ def plot_results(df):
     sns.set(style="whitegrid", rc={"axes.titlesize":16,"axes.labelsize":14})
     fig, axes = plt.subplots(1, 2, figsize=(14, 6))
 
-    blue_palette = sns.color_palette("Blues_r", n_colors=2)
-
-    sns.lineplot(x='Entries', y='InsertTime', hue='Filter', style='Filter', markers=True, dashes=False, data=df, palette=blue_palette, ax=axes[0])
-    axes[0].set_title('Insertion Time')
+    sns.lineplot(x='Entries', y='InsertTime', hue='Filter', style='Filter', markers=True, dashes=False, data=df, ax=axes[0])
+    axes[0].set_title('Insertion')
     axes[0].set_xlabel('Entries')
-    axes[0].set_ylabel('Insert Time (s)')
+    axes[0].set_ylabel('Execution Time (s)')
     axes[0].legend(loc='upper left', title='Filter')
 
-    sns.lineplot(x='Entries', y='QueryTime', hue='Filter', style='Filter', markers=True, dashes=False, data=df, palette=blue_palette, ax=axes[1])
-    axes[1].set_title('Query Time')
+    sns.lineplot(x='Entries', y='QueryTime', hue='Filter', style='Filter', markers=True, dashes=False, data=df, ax=axes[1])
+    axes[1].set_title('Querying')
     axes[1].set_xlabel('Entries')
-    axes[1].set_ylabel('Query Time (s)')
+    axes[1].set_ylabel('Execution Time (s)')
     axes[1].legend(loc='upper left', title='Filter')
 
     plt.tight_layout()
